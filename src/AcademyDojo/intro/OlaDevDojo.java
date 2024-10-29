6752d5d0112478d39bcfa632cb7b1331a4d536c1
Pringles <pedroworkpartner@outlook.com>
2024-10-28 22:31:38 -0300

    Initial commit

---
+++ src/AcademyDojo/intro/OlaDevDojo.java
@@ -0,0 +1,7 @@
+package AcademyDojo.intro;
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/AcademyDojo/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package AcademyDojo.javacore.introducaoclasses.test;
+
+import AcademyDojo.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Pedro";
+        estudante.idade = 20;
+        estudante.sexo = 'M';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+
+    }
+}
